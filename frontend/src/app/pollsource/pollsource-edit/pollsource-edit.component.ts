0b7efbf3dfc492925e31547bfb2ba29bd3137898
Benoit Bordigoni <benoit.bordigoni@ieva.io>
2017-11-07 21:38:17 +0100

    handle pollsource creation

--- frontend/src/app/pollsource/pollsource-edit/pollsource-edit.component.ts
+++ frontend/src/app/pollsource/pollsource-edit/pollsource-edit.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import {PollSource} from '../pollsource.model';
+import {PollsourceService} from '../pollsource.service';
 
 @Component({
   selector: 'app-pollsource-edit',
@@ -7,10 +11,35 @@ import {Component, OnInit} from '@angular/core';
 })
 export class PollsourceEditComponent implements OnInit {
 
-  constructor() {
+  private pollSource$: Observable<PollSource>;
+  private clientId: string;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private service: PollsourceService) {
   }
 
   ngOnInit() {
+    this.pollSource$ = this.route.paramMap.switchMap((params) => {
+      this.clientId = params.get('clientId');
+      let id = params.get('id');
+      if (id == 'new') {
+        return Observable.create(obs => {
+          obs.next({clientId: this.clientId, delay: 1000});
+          obs.complete()
+        });
+      } else {
+        return this.service.get(this.clientId, id);
+      }
+    })
+  }
+
+  save(pollSource: PollSource) {
+    this.service.save(this.clientId, pollSource).toPromise().then(() => this.back());
+  }
+
+  back() {
+    this.router.navigate(['/client', this.clientId, 'pollsource']);
   }
 
 }
