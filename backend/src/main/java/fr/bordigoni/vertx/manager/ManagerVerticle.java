92b57eb270978d20646b6450d5dc70d2c3223b98
Benoit Bordigoni <benoit.bordigoni@ieva.io>
2017-11-13 10:24:41 +0100

    manage startup using rxjava

--- backend/src/main/java/fr/bordigoni/vertx/manager/ManagerVerticle.java
+++ backend/src/main/java/fr/bordigoni/vertx/manager/ManagerVerticle.java
@@ -1,14 +1,9 @@
 package fr.bordigoni.vertx.manager;
 
 import fr.bordigoni.vertx.manager.db.DbVerticle;
-import io.vertx.config.ConfigRetriever;
-import io.vertx.config.ConfigRetrieverOptions;
-import io.vertx.config.ConfigStoreOptions;
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
-import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.core.AbstractVerticle;
+import io.vertx.rxjava.core.Vertx;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,66 +13,46 @@ import org.slf4j.LoggerFactory;
 public class ManagerVerticle extends AbstractVerticle {
 
   private static final Logger LOG = LoggerFactory.getLogger(ManagerVerticle.class);
-  private static final String SYS_PROP_CONFIG = "config";
+//  private static final String SYS_PROP_CONFIG = "config";
 
-  public static final void main(String... args) {
+  public static void main(String... args) {
 
+    Vertx.vertx().rxDeployVerticle(ManagerVerticle.class.getName())
+      .subscribe(id ->
+        LOG.info("OK"), err -> LOG.error("Unable to deploy verticle : ", err));
 
-    Vertx.vertx().deployVerticle(new ManagerVerticle(), ar->{
-      if(ar.succeeded()) {
-        LOG.info("OK");
-      } else {
-        ar.cause().printStackTrace(System.err);
-      }
-    });
   }
 
+
   @Override
-  public void start(final Future<Void> startFuture) throws Exception {
+  public void start(Future<Void> startFuture) throws Exception {
 
 
-    String configPath = System.getProperty(SYS_PROP_CONFIG);
-
-    Future<DeploymentOptions> getConfig = Future.future();
-
-    if (configPath != null) {
-      ConfigRetriever configRetriever = ConfigRetriever.create(vertx,
-        new ConfigRetrieverOptions().addStore(
-          new ConfigStoreOptions()
-            .setType("file")
-            .setConfig(new JsonObject().put("path", configPath))));
-
-      configRetriever.getConfig(handler -> {
-        final DeploymentOptions options = new DeploymentOptions();
-        if (handler.succeeded()) {
-          options.setConfig(handler.result());
-          getConfig.complete(options);
-        } else {
-          getConfig.fail(handler.cause());
-        }
-      });
-
-    } else {
-      LOG.warn("Could not find system property '{}'. Add -D{}=path/to/config to your java command, default configuration is applied", SYS_PROP_CONFIG, SYS_PROP_CONFIG);
-      getConfig.complete(new DeploymentOptions());
-    }
+//    String configPath = System.getProperty(SYS_PROP_CONFIG);
+//    Single<DeploymentOptions> options$;
+//
+//    if (configPath != null) {
+//      ConfigRetriever configRetriever = ConfigRetriever.create(vertx,
+//        new ConfigRetrieverOptions().addStore(
+//          new ConfigStoreOptions()
+//            .setType("file")
+//            .setConfig(new JsonObject().put("path", configPath))));
+//
+//      options$ = configRetriever.rxGetConfig()
+//        .map(config -> new DeploymentOptions().setConfig(config));
+//    } else {
+//      options$ = Single.just(new DeploymentOptions());
+//      LOG.warn("Cloud not config using -D{}=/path/to/config.json. Property not set ?", SYS_PROP_CONFIG);
+//    }
 
 
-    getConfig.compose(options -> this.vertx.deployVerticle(DbVerticle.class.getName(), options, dbDeploy -> {
-      if (dbDeploy.succeeded()) {
-        LOG.info("Verticle deployed with id : {}", dbDeploy.result());
-        this.vertx.deployVerticle(ManagerApiVerticle.class.getName(), apiDeploy -> {
-          if (apiDeploy.succeeded()) {
-            LOG.info("Verticle deployed with id : {}", apiDeploy.result());
-            startFuture.complete();
-          } else {
-            startFuture.fail(apiDeploy.cause());
-          }
-        });
-      } else {
-        startFuture.fail(dbDeploy.cause());
-      }
-    }), Future.succeededFuture());
+    this.vertx
+      .rxDeployVerticle(DbVerticle.class.getName()).doOnSuccess(id -> LOG.info("DbVerticle deployed with id {}", id))
+      .flatMap(options -> vertx.rxDeployVerticle(ManagerApiVerticle.class.getName()).doOnSuccess(id -> LOG.info("ApiVerticle deployed with id {}", id)))
+      .subscribe(id -> {
+        LOG.info("ManagerVerticle deployed with id : {}", id);
+        startFuture.complete();
+      }, startFuture::fail);
 
 
   }
